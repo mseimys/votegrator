58c54d6b0213681408c19f8d6171f5f4db29c078
Matas Seimys <matas.seimys@gmail.com>
2022-12-27 07:46:20 +0200

    Separate subscribe

--- consumer.py
+++ consumer.py
@@ -1,12 +1,11 @@
-import time
 from json import loads
+from time import sleep
 
 from kafka import KafkaConsumer
 
 from constants import TOPIC
 
 consumer = KafkaConsumer(
-    TOPIC,
     bootstrap_servers=["localhost:9092"],
     auto_offset_reset="earliest",
     enable_auto_commit=True,
@@ -14,6 +13,28 @@ consumer = KafkaConsumer(
     value_deserializer=lambda x: loads(x.decode("utf-8")),
 )
 
+
+# def on_assign_callback(
+#     consumer,
+#     partitions,
+# ):
+#     """Modify assigned partitions to read up to MAX_HISTORY old messages"""
+#     for partition in partitions:
+#         min_offset, max_offset = consumer.get_watermark_offsets(partition)
+#         # desired_offset = max_offset - MAX_HISTORY
+#         # if desired_offset <= min_offset:
+#         #     desired_offset = OFFSET_BEGINNING
+#         print("min_offset, max_offset", min_offset, max_offset)
+#         partition.offset = -2
+#     consumer.assign(partitions)
+
+consumer.subscribe([TOPIC])
+consumer.poll()
+consumer.seek_to_beginning()
+
+i = 1
+
 for message in consumer:
     message = message.value
-    print("GOT:", message)
+    print(f"{i} GOT:", message)
+    i += 1
